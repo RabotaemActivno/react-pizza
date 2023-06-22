da33618c7fa407219cfcfb80d3d8fad85bc0d566
RabotaemActivno <fajzovigor5@gmail.com>
2023-06-22 15:07:32 +0300

    -Searching

---
+++ src/components/Search/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from './Serach.module.scss'
+import { ReactComponent as SearchSVG } from './search_icon.svg'
+import { ReactComponent as CloseSVG } from './close_icon.svg'
+
+export function Search({searchValue, setSearchValue}) {
+    return (
+        <div className={styles.root}>
+            <SearchSVG className={styles.icon}/>
+            <input 
+                className={styles.input} 
+                placeholder="Поиск пиццы..." 
+                value={searchValue} 
+                onChange={(e)=>setSearchValue(e.target.value)}/>
+            {searchValue && <CloseSVG onClick={()=>setSearchValue('')} className={styles.close_icon}/>}
+        </div>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 import { PizzaBlock } from '../components/PizzaBlock/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
 
-export function Home() {
+export function Home({searchValue}) {
 
     const [items, setItems] = useState([])
     const [isLoading, setIsLoading] = useState(true)
@@ -22,17 +22,20 @@ export function Home() {
         const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc'
         const sortBy = sortType.sortProperty.replace('-','')
         const category = categoryId > 0 ? `category=${categoryId}` : ''
+        const search = searchValue ? `&search=${searchValue}` : ''
         
         fetch(`https://648086cff061e6ec4d49700f.mockapi.io/items?${category
-            }&sortBy=${sortBy}&order=${order}`)
+            }&sortBy=${sortBy}&order=${order}${search}`)
             .then(res => res.json())
             .then(json => {
                 setItems(json)
                 setIsLoading(false)
             });
         window.scrollTo(0, 0)
-    }, [categoryId, sortType])
+    }, [categoryId, sortType, searchValue])
 
+    const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj}/>)
+    const skeletons =[...new Array(6)].map((_, i) => <Skeleton key={i} />)
 
     return (
         <div className="container">
@@ -43,8 +46,7 @@ export function Home() {
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
                 {
-                    isLoading ? [...new Array(6)].map((_, i) => <Skeleton key={i} />)
-                        : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)
+                    isLoading ? skeletons : pizzas
                 }
             </div>
         </div>
