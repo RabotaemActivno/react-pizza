dacfeaa10e3db28a7838fe5aa1e04ab41b9d2a7e
RabotaemActivno <fajzovigor5@gmail.com>
2023-06-23 14:02:49 +0300

    -pagination

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -3,16 +3,16 @@ import styles from './Serach.module.scss'
 import { ReactComponent as SearchSVG } from './search_icon.svg'
 import { ReactComponent as CloseSVG } from './close_icon.svg'
 
-export function Search({searchValue, setSearchValue}) {
+export function Search({ searchValue, setSearchValue }) {
     return (
         <div className={styles.root}>
-            <SearchSVG className={styles.icon}/>
-            <input 
-                className={styles.input} 
-                placeholder="Поиск пиццы..." 
-                value={searchValue} 
-                onChange={(e)=>setSearchValue(e.target.value)}/>
-            {searchValue && <CloseSVG onClick={()=>setSearchValue('')} className={styles.close_icon}/>}
+            <SearchSVG className={styles.icon} />
+            <input
+                className={styles.input}
+                placeholder="Поиск пиццы..."
+                value={searchValue}
+                onChange={(e) => setSearchValue(e.target.value)} />
+            {searchValue && <CloseSVG onClick={() => setSearchValue('')} className={styles.close_icon} />}
         </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@ import { Categories } from '../components/Categories';
 import { useState, useEffect } from 'react';
 import { PizzaBlock } from '../components/PizzaBlock/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
+import { Pagination } from '../components/Pagination';
 
-export function Home({searchValue}) {
+export function Home({ searchValue }) {
 
     const [items, setItems] = useState([])
     const [isLoading, setIsLoading] = useState(true)
     const [categoryId, setCatigoryId] = useState(0)
+    const [currentPage, setCurrentPage] = useState(1)
     const [sortType, setSortType] = useState({
         name: 'популярности',
         sortProperty: 'rating'
@@ -20,11 +22,11 @@ export function Home({searchValue}) {
         setIsLoading(true)
 
         const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc'
-        const sortBy = sortType.sortProperty.replace('-','')
+        const sortBy = sortType.sortProperty.replace('-', '')
         const category = categoryId > 0 ? `category=${categoryId}` : ''
         const search = searchValue ? `&search=${searchValue}` : ''
-        
-        fetch(`https://648086cff061e6ec4d49700f.mockapi.io/items?${category
+
+        fetch(`https://648086cff061e6ec4d49700f.mockapi.io/items?page=${currentPage}&limit=4&${category
             }&sortBy=${sortBy}&order=${order}${search}`)
             .then(res => res.json())
             .then(json => {
@@ -32,10 +34,10 @@ export function Home({searchValue}) {
                 setIsLoading(false)
             });
         window.scrollTo(0, 0)
-    }, [categoryId, sortType, searchValue])
+    }, [categoryId, sortType, searchValue, currentPage])
 
-    const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj}/>)
-    const skeletons =[...new Array(6)].map((_, i) => <Skeleton key={i} />)
+    const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)
+    const skeletons = [...new Array(6)].map((_, i) => <Skeleton key={i} />)
 
     return (
         <div className="container">
@@ -49,6 +51,7 @@ export function Home({searchValue}) {
                     isLoading ? skeletons : pizzas
                 }
             </div>
+            <Pagination onChangePage={number => setCurrentPage(number)}/>
         </div>
     )
 }
